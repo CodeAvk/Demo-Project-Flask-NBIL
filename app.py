2b13bf07fec98db2f00ee02cbbd2d2e58f801163
Abhisek0770 <abhisek.s@nextbiginnovationlabs.com>
2023-12-20 15:03:32 +0530

    Working on Adding Image to Database

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+# from werkzeug.utils import secure_filename
+import os
 
 
 app = Flask(__name__)
@@ -14,25 +16,60 @@ class Todo(db.Model):
     title=db.Column(db.String(200),nullable=False)
     desc=db.Column(db.String(500),nullable=False)
     date_created=db.Column(db.DateTime,default=datetime.utcnow)
+    image = db.Column(db.String(200))
 
     def __repr__(self)-> str:
         return f"{self.sno} - {self.title}"
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def hello_world():
-    todo=Todo(title="First todo",desc="Start Learning Go")
-    db.session.add(todo)
-    db.session.commit()
+    if request.method=='POST':
+        # print(request.form['image'])
+        title=request.form['title']
+        desc=request.form['desc']
+        image = request.form['image'] 
+        print(image)
+        # print(request.files['image'])
+        
+        # # Save the file and get the file path
+        # if image and image.filename != '':
+        #     filename = secure_filename(image.filename)
+        #     image.save(os.path.join('static/uploads', filename))  # Save the file to 'static/uploads' directory
+        #     image_path = f"uploads/{filename}"  # Relative path to the saved image
+        # else:
+        #     image_path = None
+
+        # Create and add the Todo to the database
+       
+        todo=Todo(title=f"{title}",desc=f"{desc}",image=f"{image}")
+        db.session.add(todo)
+        db.session.commit()
     allTodo=Todo.query.all()
-    print(allTodo)
+    # print(allTodo)
 
-    return render_template("index.html")
-
+    return render_template("index.html", allTodo=allTodo)
 @app.route('/show')
 def products():
     allTodo=Todo.query.all()
     print(allTodo)
     return f'Here is all to do section --- \n{allTodo}'
+
+@app.route('/update/<int:sno>', methods=['GET', 'POST'])
+def update(sno):
+    todo = Todo.query.get_or_404(sno)
+    if request.method == 'POST':
+        todo.title = request.form['title']
+        todo.desc = request.form['desc']
+        db.session.commit()
+        return redirect('/')
+    return render_template('/', todo=todo)
+
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    todo_to_delete = Todo.query.get_or_404(sno)
+    db.session.delete(todo_to_delete)
+    db.session.commit()
+    return redirect('/')
 
 if __name__ == "__main__":
     with app.app_context():
